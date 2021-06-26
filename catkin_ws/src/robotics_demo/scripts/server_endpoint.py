dc87ce72745e4dc02dac4ba83912dafda532f774
Takashi Mori <tmori@esm.co.jp>
2021-06-26 17:35:15 +0900

    add ev3 demo

--- catkin_ws/src/robotics_demo/scripts/server_endpoint.py
+++ catkin_ws/src/robotics_demo/scripts/server_endpoint.py
@@ -3,8 +3,8 @@
 import rospy
 
 from ros_tcp_endpoint import TcpServer, RosPublisher, RosSubscriber, RosService, UnityService
-from robotics_demo.msg import PosRot
 from robotics_demo.msg import Actuator
+from robotics_demo.msg import Sensor
 from robotics_demo.srv import PositionService, ObjectPoseService
 
 def main():
@@ -15,10 +15,8 @@ def main():
     rospy.init_node(ros_node_name, anonymous=True)
     
     tcp_server.start({
-        'pos_rot': RosPublisher('pos_rot', PosRot, queue_size=10),
+        'sensor': RosPublisher('sensor', Sensor, queue_size=10),
         'actuator': RosSubscriber('actuator', Actuator, tcp_server),
-        'pos_srv': RosService('pos_srv', PositionService),
-        'obj_pose_srv': UnityService('obj_pose_srv', ObjectPoseService, tcp_server),
     })
     
     rospy.spin()
